5e239ee36b97a67de6b521b41d5f2e958e25234f
sonu267 <65347096+sonu267@users.noreply.github.com>
2024-03-07 23:27:00 +0530

    Add files via upload

---
+++ area of circle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    float a, r;
+    
+    printf("enter the numbers");
+    scanf("%f", &r);
+    a = 3.14 * r*r;
+    printf("here is the radius %f", a);
+
+
+    return 0;
+}
